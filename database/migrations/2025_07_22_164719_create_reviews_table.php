990a7505546adf6c68dffcdc459277f0b3211b02
dev/vinh <thanhvinhly369@gmail.com>
2025-07-29 14:45:52 +0700

    table database

---
+++ database/migrations/2025_07_22_164719_create_reviews_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('jewelries_id')->constrained('jewelries')->onDelete('cascade');
+            $table->text('content');
+            $table->integer('rating');
+            $table->timestamps();
+            $table->tinyInteger('is_deleted')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
